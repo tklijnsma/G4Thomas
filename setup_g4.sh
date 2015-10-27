e8ace0d829d04c7d4b45ae9ef7c82108a9bfefba
Thomas Klijnsma <klthomas@student.ethz.ch>
2015-10-27 09:33:45 +0100

    added setup script

---
+++ setup_g4.sh
@@ -0,0 +1,4 @@
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6/setup.sh
+source /afs/cern.ch/sw/lcg/external/geant4/10.0.p01/x86_64-slc6-gcc47-opt/CMake-setup.sh
+export CXX=/afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6/bin/g++
+export CC=/afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6/bin/gcc
